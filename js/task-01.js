d320ecb15983d9eeb61a0b56993bd72dadbb6d90
AleksandraSzendzielorz <kontaktowyoficjalny@gmail.com>
2024-01-21 13:32:57 +0100

    second commit

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,15 @@
-document.addEventListener("WebsiteLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const categoriesList = document.querySelectorAll("#categories .item");
   const numberOfCategories = categoriesList.length;
   console.log("Number of categories:", numberOfCategories);
 
   categoriesList.forEach(function (category) {
-    const categoryName = category.querySelector("h2").textContent;
+    const categoryName = category.querySelector("h2").textContent.trim();
 
     const categoryElements = category.querySelectorAll("ul > li");
     const numberOfElements = categoryElements.length;
 
-    // Wypisuje wyniki w konsoli
-    console.log("Category:", categoryName);
+    console.log("\nCategory:", categoryName);
     console.log("Elements:", numberOfElements);
   });
 });
